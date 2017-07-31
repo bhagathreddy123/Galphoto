c553e1a59961be27141a2170256c4c9a6e473d4d
bhagath <bhagath@getcustomexposure.com>
2017-07-31 09:51:53 +0530

    adding lightbox

--- public/controllers/gallery.controller.js
+++ public/controllers/gallery.controller.js
@@ -1,10 +1,19 @@
  angular.module('galPhoto')
-.controller('GalleryCtrl',['$scope','instagram',function($scope,instagram){
+.controller('GalleryCtrl',['$scope','instagram','Lightbox',function($scope,instagram,Lightbox){
 	// console.log($scope);
 	$scope.images = [];
+	var imgArray = [];
 instagram.fetchPopular(function(data){
 	// console.log(data);
 	$scope.images = data;
+	angular.forEach(data,function(value,key){
+		imgArray.push(value.images.standard_resolution);
+	});
+	$scope.openLightboxModel = function(index){
+		// Lightbox.openModel($scope.images,index);
+		Lightbox.openModel(imagArray,index);
 
+	}
 })
 }])
+
--- public/services/instagram.service.js
+++ public/services/instagram.service.js
@@ -4,7 +4,8 @@ angular.module('galPhoto')
 	return {
 		fetchPopular: function(callback){
 			var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK', {
-				client_id: 'c8f7f1ecd8674739b2971eafbc9c7320'
+				// client_id: 'c8f7f1ecd8674739b2971eafbc9c7320'
+				client_id: '576cb3d40e7e42a3accdf23797762541'
 			},{
 				fetch:{method:'JSONP'}
 			});
